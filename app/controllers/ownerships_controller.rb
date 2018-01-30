736c7853de49ea645057c68b0aa9e0a8784baf3c
yuusuke.nishimura <west246x@gmail.com>
2018-01-30 09:23:36 +0000

    have

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -15,6 +15,11 @@ class OwnershipsController < ApplicationController
       current_user.want(@item)
       flash[:success] = '商品を Want しました。'
     end
+    
+    if params[:type] == 'Have'
+      current_user.have(@item)
+      flash[:success] = '商品を Have しました。'
+    end
 
     redirect_back(fallback_location: root_path)
   end
@@ -25,6 +30,11 @@ class OwnershipsController < ApplicationController
     if params[:type] == 'Want'
       current_user.unwant(@item) 
       flash[:success] = '商品の Want を解除しました。'
+    end
+    
+    if params[:type] == 'Have'
+      current_user.unhave(@item) 
+      flash[:success] = '商品の Have を解除しました。'
     end
 
     redirect_back(fallback_location: root_path)
